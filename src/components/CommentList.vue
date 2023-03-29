5232dc1132311aa9eda95666bb5d08f8ef837a27
tapashpaulseopage1 <tapashpaulseopage1@gmail.com>
2023-03-29 13:50:14 +0600

    update again with comment dialog

---
+++ src/components/CommentList.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <v-list-item
+      class="d-flex"
+      v-for="comment in commentList"
+      :key="comment.id"
+    >
+      <v-avatar size="36" color="red" class="pa-0 mt-n10">
+        <span class="white--text font-weight-normal font">
+          {{ comment.label }}
+        </span>
+      </v-avatar>
+
+      <v-list-item-content class="pa-1">
+        <v-list-item-title class="d-flex justify-start">
+          <span class="text-body-1 font-weight-bold font px-2">{{
+            comment.name
+          }}</span>
+          <span
+            class="text-caption font-weight-bold gray--text pt-1 font span"
+            >{{ newDateAndTime }}</span
+          >
+        </v-list-item-title>
+
+        <input name="text" type="text" value="call dynamic domain name done" />
+
+        <v-list-item-title class="comment-list">
+          <v-icon size="18" color="gray"> mdi-emoticon-happy-outline </v-icon>
+
+          <v-btn
+            @click="showInput = true"
+            icon
+            class="text-body-1 font-weight-normal text-decoration-underline mx-2 py-0 font"
+          >
+            Reply
+          </v-btn>
+          <v-btn
+            @click="showInput = false"
+            icon
+            class="text-body-1 font-weight-normal text-decoration-underline mx-2 py-0 font"
+          >
+            Delete
+          </v-btn>
+          <v-btn
+            @click="showCount = !showCount"
+            icon
+            class="text-body-1 font-weight-normal mx-2 py-0 font"
+          >
+            <v-icon size="18" color="blue"> mdi-paperclip </v-icon>
+          </v-btn>
+          <span v-if="showCount" class="pt-3 font-weight-bold text-sm">5</span>
+        </v-list-item-title>
+        <v-list-item-subtitle v-if="showInput">
+          <input
+            name="text"
+            type="text"
+            value="call dynamic domain name done"
+          />
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: new Date().toLocaleString("en-us", {
+        month: "short",
+        year: "numeric",
+        day: "numeric",
+      }),
+
+      tt: new Date().toLocaleTimeString(navigator.language, {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+
+      showInput: false,
+      showCount: false,
+      commentList: [
+        {
+          id: 1,
+          label: "MS",
+          name: "new developer join with us",
+        },
+        {
+          id: 2,
+          label: "MD",
+          name: "new developer join with us",
+        },
+        {
+          id: 3,
+          label: "SD",
+          name: "new developer join with us",
+        },
+        {
+          id: 4,
+          label: "DM",
+          name: "new developer join with us",
+        },
+      ],
+    };
+  },
+
+  computed: {
+    newDateAndTime() {
+      return this.date + " at " + this.tt;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.comment-list {
+  display: grid;
+  grid-template-columns: 30px 60px 60px 40px 30px;
+}
+
+input {
+  background: #fff;
+  padding: 10px;
+  box-shadow: 0 0 2px gray;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: 500;
+}
+</style>
--- src/components/SecondBox.vue
+++ src/components/SecondBox.vue
@@ -24,30 +24,87 @@
             <v-list-item-title class="text font pl-2">
               {{ list.text }}
             </v-list-item-title>
-            <v-list-item-subtitle class="d-flex justify-end sub"
-              ><v-icon class="text-body-1 px-2">{{ list.icon1 }}</v-icon>
+            <v-list-item-subtitle class="d-flex justify-end sub">
+              <v-icon class="text-body-1 px-2">{{ list.icon1 }}</v-icon>
               <v-icon class="text-body-1 px-2">{{ list.icon2 }}</v-icon>
             </v-list-item-subtitle>
           </v-list-item>
         </v-list>
       </v-card-text>
       <v-card-title class="tagg">
-        <v-btn icon>
-          <v-avatar size="24" color="blue">
-            <v-icon color="white" class="text-body-1 font-weight-regular">
-              mdi-chevron-left
-            </v-icon>
-          </v-avatar>
-        </v-btn>
+        <div>
+          <v-btn icon @click="openDialog">
+            <v-avatar size="24" color="blue">
+              <v-icon
+                color="white"
+                class="text-body-1 font-weight-regular"
+                v-if="isActive"
+              >
+                mdi-chevron-left
+              </v-icon>
+              <v-icon
+                color="white"
+                class="text-body-1 font-weight-regular"
+                v-else
+              >
+                mdi-chevron-right
+              </v-icon>
+            </v-avatar>
+          </v-btn>
+
+          <v-dialog v-model="dialog" width="750px">
+            <v-card class="pa-4 card">
+              <v-card-text class="pa-4 card-text">
+                <v-list class="pa-0 card-text">
+                  <v-list-item
+                    class="d-flex justify-start first"
+                    color="transparant"
+                  >
+                    <v-avatar size="30" color="blue" class="pa-0">
+                      <span class="white--text font-weight-normal font">
+                        RS
+                      </span>
+                    </v-avatar>
+                    <v-text-field
+                      type="file"
+                      class="text-body-1 font-weight-medium pa-2"
+                      append-icon="mdi-check-circle"
+                      color="primary"
+                      dense
+                      solo
+                      hide-details
+                    />
+                  </v-list-item>
+                </v-list>
+
+                <CommentList />
+              </v-card-text>
+
+              <v-card-actions class="d-flex justify-end py-1">
+                <v-btn color="primary" text @click="dialog = false"
+                  >Close</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
       </v-card-title>
     </v-card>
   </div>
 </template>
 
 <script>
+import CommentList from "./CommentList.vue";
+
 export default {
   data() {
     return {
+
+
+
+      isActive: true,
+      dialog: true,
+
       content2: [
         {
           id: 1,
@@ -109,14 +166,51 @@ export default {
       ],
     };
   },
+
+  components: {
+    CommentList,
+  },
+
+
+
+  methods: {
+    openDialog() {
+      this.dialog = true;
+      this.isActive = !this.isActive;
+    },
+  },
 };
 </script>
 
 <style scoped>
+.v-dialog {
+  border: 10px solid gold !important;
+}
+
+.card {
+  border: 2px solid #1c96e7;
+  border-radius: 2%;
+  background: #f4f5f7;
+}
+
+.card-text {
+  background: #f4f5f7;
+}
+
 .v-list-item {
   min-height: 30px !important;
+
   /* border-bottom: 1px solid rgba(128, 128, 128, 0.272); */
 }
+
+.v-input__prepend-outer {
+  margin-right: 0 !important;
+}
+
+.span {
+  color: gray;
+}
+
 .list-item {
   border-top: 1px solid rgba(128, 128, 128, 0.251);
 }
@@ -127,6 +221,20 @@ export default {
   left: -10%;
   z-index: 99;
 }
+
+.hidden-sm-and-down .v-icon {
+  color: white !important;
+}
+
+input {
+  background: #fff;
+  padding: 10px;
+  box-shadow: 0 0 2px gray;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: 500;
+}
+
 .text {
   color: #777777;
   font-size: 14px;
